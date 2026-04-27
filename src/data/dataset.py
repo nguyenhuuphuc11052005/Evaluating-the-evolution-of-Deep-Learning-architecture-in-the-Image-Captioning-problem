f147f94546c5eee9ad45d336f7fe46e2609c30a2
Nguyen Huu Phuoc <nguyenhuuphuoc11052005@gmail.com>
2026-04-27 09:09:03 +0700

    add folder

---
+++ src/data/dataset.py
@@ -0,0 +1,76 @@
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
+from PIL import Image
+from pycocotools.coco import COCO
+import torchvision.transforms as transforms
+
+class CocoDataset(Dataset):
+    def __init__(self, root_dir, ann_file, vocab, transform=None, limit=50000):
+        self.root_dir = root_dir
+        self.coco = COCO(ann_file)
+        self.vocab = vocab
+        self.transform = transform
+        
+        # Lấy tất cả ID của caption, sau đó giới hạn ở con số 50,000
+        self.ids = list(self.coco.anns.keys())[:limit]
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, index):
+        ann_id = self.ids[index]
+        caption = self.coco.anns[ann_id]['caption']
+        img_id = self.coco.anns[ann_id]['image_id']
+        path = self.coco.loadImgs(img_id)[0]['file_name']
+
+        # Load ảnh
+        img = Image.open(os.path.join(self.root_dir, path)).convert("RGB")
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        # Chuyển text thành chuỗi số: <sos> + [chuỗi index] + <eos>
+        numericalized_caption = [self.vocab.stoi["<sos>"]]
+        numericalized_caption += self.vocab.numericalize(caption)
+        numericalized_caption.append(self.vocab.stoi["<eos>"])
+
+        return img, torch.tensor(numericalized_caption)
+
+# Transform cơ bản cho Transfer Learning 
+def get_transforms():
+    return transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], # Chuẩn ImageNet
+                             std=[0.229, 0.224, 0.225])
+    ])
+
+class CapsCollate:
+    def __init__(self, pad_idx):
+        self.pad_idx = pad_idx
+
+    def __call__(self, batch):
+        # Batch là danh sách các tuple (image, caption)
+        imgs = [item[0].unsqueeze(0) for item in batch]
+        imgs = torch.cat(imgs, dim=0)
+        
+        targets = [item[1] for item in batch]
+        # Pad sequence để các câu bằng nhau
+        targets = pad_sequence(targets, batch_first=True, padding_value=self.pad_idx)
+
+        return imgs, targets
+
+def get_loader(root_dir, ann_file, vocab, transform, batch_size=32, num_workers=2, limit=50000):
+    dataset = CocoDataset(root_dir, ann_file, vocab, transform, limit)
+    pad_idx = vocab.stoi["<pad>"]
+
+    loader = DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        shuffle=True, # Trộn ảnh lên cho mỗi epoch
+        collate_fn=CapsCollate(pad_idx=pad_idx)
+    )
+    return loader
